8c9c31b99de81cae54b4225f2d979f1c76e228ef
Andrey Kuznetsov <nice.andrey013@gmail.com>
2023-05-17 02:37:59 +0300

    Функции, применение

---
+++ 1_basical_python/lesson_6.function/lesson_6.py
@@ -0,0 +1,14 @@
+# def say_hello():
+#     print("Hello my dear")
+
+
+# def say_goodbye():
+#     print("Goodbye my dear")
+
+# say_hello()
+# say_goodbye()
+
+def mult(x=3):
+    return x*(x+x)
+
+print(mult())
