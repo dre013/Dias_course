c97710cd56915a93fd7c484587aa1bad919d1928
Andrey Kuznetsov <nice.andrey013@gmail.com>
2023-05-16 04:26:36 +0300

    Списки и их методы

---
+++ 1_basical_python/lesson_4.list_metods/lesson_4.py
@@ -0,0 +1,55 @@
+number = [1,2,3,4,5]
+
+objects = ['Person',True,1,2,3,5.6]
+
+# print(number)
+# print(name)
+# print(objects)
+
+# number2=[5]*6
+# print(number2)
+
+# name = ['Tom','Bob'] * 6
+# print(name)
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print()
+# print(name[-1])
+# print(name[-2])
+# print(name[-3])
+
+# name[1] = 'Josh'
+# print(name)
+
+# i = 0
+# while i < len(name):
+#     print(name[i])
+#     i+=1
+
+name = ['Tom','Bob','Sam']
+
+# name.append('Alice')
+# print(name)
+
+# name.insert(2,'Josh')
+# print(name)
+
+# name.extend(['Mike','John'])
+# print(name)
+
+# index_start = name.index("John")
+# print(index_start)
+
+# removed_id = name.pop()
+# print(removed_id)
+
+# name.remove('Alice')
+# print(name)
+
+# name.clear()
+# print(name)
+if "Tom" in name:
+    name.remove("Alice")
+    print(name)
