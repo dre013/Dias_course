ce330d4c0081fd5544b82f0733f2f5f8f2915353
Andrey Kuznetsov <nice.andrey013@gmail.com>
2023-05-16 04:25:37 +0300

    База

---
+++ 1_basical_python/lesson_2/lesson2.py
@@ -0,0 +1,33 @@
+
+if True:
+    print("hello")
+
+if 3>2:
+    print("3 is more 2")
+
+if 3<2:
+    print("2 is more 3")
+
+is_true = True #False
+if is_true:
+    print("Yes its True")
+
+name = input("Please enter a name: ")
+if name == 'Tom':
+    print(f'Hey, {name} i know you')
+elif name == 'Bob':
+    print(f'Hey, {name} i know you')
+elif name == 'John':
+    print(f'Hey, {name} i know you')
+else:
+    print(f'I dont know you -  {name}')
+
+
+a = 5
+b = 6
+if a > b:#False
+    print("Yes")
+elif a == b:#False
+    print("No")
+else:#True
+    print("I don't know'")
