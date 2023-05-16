017be6ab2af022a1a06ab63480db744eaf6dfc20
Andrey Kuznetsov <nice.andrey013@gmail.com>
2023-05-17 00:50:37 +0300

    Словари и методы

---
+++ 1_basical_python/lesson_5.dictionary/lesson_5.py
@@ -0,0 +1,35 @@
+# users = {1:"Dias",2:"Tom"}
+# emails = {"dias@example.com":"Dias"}
+
+# obj = {}
+# obj1 = dict()
+
+# user_list = [
+#     ['+123456','Tom'],
+#     ['+1234567','Bob'],
+#     ['+1234568','Dias'],
+# ]
+# user_dict = dict(user_list)
+
+# print(user_dict)
+# print(user_list)
+
+# # print(users[1])
+# # users[1] = 'Josh'
+# # print(users[1])
+# #users[4] 
+
+# print(users[4])
+
+users = {1:"Dias",
+         2:"Tom",
+         3:"Joe"}
+
+# key = 2
+# uses = users.pop(key)
+# print(uses) 
+
+#users.clear()
+
+for key in users:
+    print(f'Index: {key} User: {users[key]}')
