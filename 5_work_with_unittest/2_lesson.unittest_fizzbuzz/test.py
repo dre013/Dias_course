e4e6ea7c309f4c6da1850fd42a397fbceb622ea6
Andrey Kuznetsov <nice.andrey013@gmail.com>
2023-09-20 23:43:45 +0300

    Работа с юнит тестами

--- 5_work_with_unittest/2_lesson.unittest_fizzbuzz/test.py
+++ 5_work_with_unittest/2_lesson.unittest_fizzbuzz/test.py
@@ -1,22 +1,24 @@
 import unittest
 import lesson
 
+
 class FizzbuzzTestCase(unittest.TestCase):
 
     def test_fizz(self):
         number = 3
-        result = lesson.getReply(number) # Fizz
-        self.assertEqual(result,'Fizz')
+        result = lesson.getReply(number)  # Fizz
+        self.assertEqual(result, 'Fizz')
 
     def test_buzz(self):
         number = 5
-        result = lesson.getReply(number) # Buzz
-        self.assertEqual(result,'Buzz')
+        result = lesson.getReply(number)  # Buzz
+        self.assertEqual(result, 'Buzz')
 
     def FizzBuzz(self):
         number = 15
-        result = lesson.getReply(number) # Fizzbuzz
-        self.assertEqual(result,'FizzBuzz')
-    
+        result = lesson.getReply(number)  # Fizzbuzz
+        self.assertEqual(result, 'FizzBuzz')
+
+
 if __name__ == '__main__':
     unittest.main()
