eb79e700c2a528e154249fadfec786340f744c30
Andrey Kuznetsov <nice.andrey013@gmail.com>
2023-10-05 15:29:59 +0300

    Flask работа с сайтом

--- 9_Web_dev/1_lesson.flask/application/__init__.py
+++ 9_Web_dev/1_lesson.flask/application/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345678'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:Favorite43!@localhost:5432/testDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost:5432/testDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
